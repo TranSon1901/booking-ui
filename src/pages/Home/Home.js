6858b8b7ec9ff3dbe2869262e882dd52847b931b
transon <transon1901@gmail.com>
2022-11-21 16:05:59 +0700

    add

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,5 @@
 import Featured from "../../components/featured/Featured"
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties"
 import Header from "../../components/Header/Header"
 import NavBar from "../../components/navbar/Navbar"
 import PropertyList from "../../components/propertyList/PropertyList"
@@ -12,6 +13,8 @@ function Home(){
             <Featured />
             <h1 className="home_title">Browse by property type</h1>
             <PropertyList />
+            <h1 className="home_title">Homes guests love</h1>
+            <FeaturedProperties />
          </div>
         </>
     )
