67aa3e53eff7b4614b39c2ad008ba72be3c6d951
transon <transon1901@gmail.com>
2022-11-21 15:08:18 +0700

    add

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,15 @@
+import Featured from "../../components/featured/Featured"
 import Header from "../../components/Header/Header"
 import NavBar from "../../components/navbar/Navbar"
-
+import './home.css'
 function Home(){
     return(
         <>
          <NavBar />
          <Header />
+         <div className="homeContainer">
+            <Featured />
+         </div>
         </>
     )
 }
