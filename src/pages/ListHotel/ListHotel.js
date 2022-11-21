01fc2c963c50522c1d6038b48a6f25abe43c80fb
transon <transon1901@gmail.com>
2022-11-21 16:50:22 +0700

    add

--- src/pages/ListHotel/ListHotel.js
+++ src/pages/ListHotel/ListHotel.js
@@ -1,11 +1,29 @@
 import Header from "../../components/Header/Header"
 import NavBar from "../../components/navbar/Navbar"
-
+import './listhotel.css'
 function ListHotel(){
     return(
       <div>
         <NavBar />
         <Header type="listHotel"/>
+        <div className="listHotel_container">
+            <div className="listHotel_wrapper">
+                <div className="listHotel_Search">
+                    <h1 className="ls_title">Search</h1>
+                    <div className="ls_Item">
+                        <label>Destination</label>
+                        <input type="text"/>
+                    </div>
+                    <div className="ls_Item">
+                        <label>Check-in date</label>
+                        <input type="text"/>
+                    </div>
+                </div>
+                <div className="listHotel_Result">
+
+                </div>
+            </div>
+        </div>
       </div>
     )
 }
