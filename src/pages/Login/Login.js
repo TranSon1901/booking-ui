7d6b96d7047879bd38962ec3fe34b6a00b79dad3
transon <transon1901@gmail.com>
2022-11-25 12:31:33 +0700

    add

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,8 +1,48 @@
+import { useContext ,useState} from 'react';
+import { AuthContext } from '../../context/AuthContext';
 import './login.css'
+import axios from 'axios'
 function Login(){
-    const []
+    const [credentials, setCredentials] = useState({
+        username: undefined,
+        password: undefined,
+      });
+      const {user, loading, error, dispatch } = useContext(AuthContext)
+      const handleChange=(e)=>{
+        setCredentials(prev=>({
+            ...prev,
+            [e.target.id]:e.target.value
+        }))
+      }
+      const handleClick= async (e) =>{
+        console.log("run")
+        e.preventDefault();
+        dispatch({type:"LOGIN_START"})
+        try{
+            const res = await axios.post("/api/auth/login", credentials);
+            dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+        }catch(err){
+            dispatch({type:"LOGIN_FAILURE", payload:err.response.data})
+        }
+      }
+      console.log(user)
     return(
-      <h1>Login</h1>
+      <div className='login'>
+          <div className='login_container'>
+               <input type="text" 
+               onChange={()=>handleChange}
+               placeholder='username' 
+               id='username' 
+               className='Login_input'/>
+               <input type="password" 
+               onChange={()=>handleChange}
+               placeholder='password' 
+               id='password' 
+               className='Login_input'/>
+               <button onClick={handleClick} className='login_btn'>Login</button>
+               {error && <span>{error.message}</span>}
+          </div>
+      </div>
     )
 }
 export default Login
--- src/routes/Routers.js
+++ src/routes/Routers.js
@@ -2,6 +2,7 @@ import { Routes,Route} from 'react-router-dom'
 import Home from '../pages/Home/Home'
 import Hotel from '../pages/Hotel/Hotel'
 import ListHotel from '../pages/ListHotel/ListHotel'
+import Login from '../pages/Login/Login'
 
 const Router=()=>{
     return(
@@ -9,6 +10,7 @@ const Router=()=>{
           <Route path='/booking-ui/' element={<Home />}/>
           <Route path='/booking-ui/hotels' element={<ListHotel />}/>
           <Route path='/booking-ui/hotels/:id' element={<Hotel />}/>
+          <Route path='/booking-ui/Login' element={<Login />}/>
        </Routes>
     )
 }
